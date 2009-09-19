4620c27e4d0ac0f2ff331f75baaae281297fc5fb
kris979 <kris979@2865d96e-a144-11de-b04a-1faa45bc5d20>
2009-09-19 16:57:15 +0000

    sending email with csv 1/2

--- iRun/error.py
+++ iRun/error.py
@@ -17,22 +17,3 @@ class Err(webapp.RequestHandler):
         self.response.out.write('<html><body>')
         self.response.out.write("error get")
         self.response.out.write('</body></html>')
-        
-        
-        
-        
-#        
-#              <form action="/" method="POST">
-#      <div>
-#      Date:<input type="text" name="Date" size="10">
-#      Duration:<input type="text" name="Duration" size="5">
-#      Distance:<input type="text" name="Distance" size="5">
-#      Pace:<input type="text" name="Pace" size="5">
-#      Pace max:<input type="text" name="Pace_max" size="5">
-#      HR:<input type="text" name="HR" size="5">
-#      HR max:<input type="text" name="HR_max" size="5">
-#      Energy:<input type="text" name="Energy" size="5">
-#      TE:<input type="text" name="TE" size="5">                                        
-#      <input type="submit" value="Add">
-#      </div>
-#    </form>
--- iRun/upload.py
+++ iRun/upload.py
@@ -5,11 +5,13 @@ Created on Sep 12, 2009
 '''
 from google.appengine.ext import webapp
 from google.appengine.api import users
+from google.appengine.api import mail
 import datetime
 import model
 import logging
 
 log = logging.getLogger("simple")
+
 d = lambda x: datetime.datetime.strptime(x, '%d/%m/%y').date()
 duration = lambda x: datetime.datetime.strptime(x, '%H:%M:%S').time()
 pace = lambda x: datetime.datetime.strptime(x, '%M:%S').time()
@@ -18,7 +20,38 @@ class AddCSV(webapp.RequestHandler):
     header = None
     rows = None
 
-    def processContent(self,content):
+    def prepareExportCSVFile(self):
+        csv = ''
+        for k in model.headers.keys():
+            csv += k
+            csv += ','
+        csv = csv.rstrip(',')
+        csv += '\n'
+        q = model.Run.all()
+        q.filter('author =', users.get_current_user())
+        q.order('-date')
+        results = q.fetch(1000)
+        for run in results:
+            line = str(run.date) + ',' + str(run.duration) + ',' + str(run.distance) + ',' + str(run.hr) \
+             + ',' + str(run.hr_max) + ',' + str(run.pace) + ',' + str(run.pace_max) + ',' + str(run.te) + ',' \
+             + str(run.energy) + ',' + run.activity + '\n'
+            csv += line
+        return csv
+         
+    def sendEmail(self):
+        to_addr = users.get_current_user().email()
+        if not mail.is_email_valid(to_addr):
+            # Return an error message...
+            pass
+        message = mail.EmailMessage(attachments=[('workout.csv',self.prepareExportCSVFile())])
+        message.sender = users.get_current_user().email()
+        message.to = to_addr
+        message.subject = 'chartmyrun.appspot.com sending workout.csv'
+        message.body = """Your workout file has been successfully exported""" 
+        message.send()
+        self.redirect('/')
+
+    def prepareDataForSaving(self,content):
         rows = content.split('\n')
         newRows = []
         for row in rows:
@@ -30,40 +63,41 @@ class AddCSV(webapp.RequestHandler):
         self.header = newRows[0]
         self.rows = newRows[1:]
 
-    def save(self):
+    def addInstanceToDb(self,row):
+        run = model.Run()
+        if users.get_current_user():
+            run.author = users.get_current_user()
+        run.added = datetime.date.today()
+        run.date = d(row[0])
+        run.duration = duration(row[1])
+        run.distance = float(row[2])
+        run.hr = int(row[3])
+        run.hr_max = int(row[4])
+        run.pace = pace(row[5])
+        run.pace_max = pace(row[6])
+        run.te = float(row[7])
+        run.energy = int(row[8])
+        run.activity = row[9]
+        run.put()
+        
+    def save(self,content):
+        self.prepareDataForSaving(content)
         for row in self.rows:
-            run = model.Run()
-            if users.get_current_user():
-                    run.author = users.get_current_user()
-            run.added = datetime.date.today()
-            run.date = d(row[0])
-            run.duration = duration(row[1])
-            run.distance = float(row[2])
-            run.hr = int(row[3])
-            run.hr_max = int(row[4])
-            run.pace = pace(row[5])
-            run.pace_max = pace(row[6])
-            run.te = float(row[7])
-            run.energy = int(row[8])
-            run.activity = row[9]
-            run.put()
+            self.addInstanceToDb(row)
         
     def post(self):
         content = self.request.get('csv')
         if content:
-            self.processContent(content)
-            self.save()
+            self.save(content)
             self.redirect('/')
         else:
+            log.error('error uploading csv file')
+#            self.redirect('/error')
             self.response.out.write('<html><body>')
             self.response.out.write("<p>error when uploading a file</p>")
             self.response.out.write('</body></html>')
-        
-#    def put(self):
-#        uploaded_file = self.request.body
-#        self.response.out.write('<html><body>')
-#        if uploaded_file:
-#            self.response.out.write("<p>%s</p>" %uploaded_file)
-#        else:
-#            self.response.out.write("<p>empty put</p>")
-#        self.response.out.write('</body></html>')
+            
+    def get(self):
+        self.sendEmail()
+#        self.redirect('/err')
+    
